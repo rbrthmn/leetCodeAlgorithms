2650ae7d272e31194eb92e65931559424d555c55
rbrthmn <robertokenzohamano@gmail.com>
2024-06-18 16:00:54 -0300

    adding solution for problem n 26

---
+++ src/main/kotlin/algorithms/RemoveDuplicatesFromSortedArray.kt
@@ -0,0 +1,26 @@
+package br.com.rbthmn.algorithms
+
+import br.com.rbthmn.Problem
+import br.com.rbthmn.algorithms.utils.moveElementToLastIndex
+import java.lang.Math.random
+
+@Problem(26)
+fun removeDuplicates(nums: IntArray): Int {
+    var pivot = nums.last()
+    var k = 1
+    var index = nums.size - 1
+
+    if (nums.size > 1) {
+        while (index >= 0) {
+            if (nums[index] == pivot) {
+                moveElementToLastIndex(nums, index)
+            } else {
+                pivot = nums[index]
+                k++
+            }
+            index--
+        }
+    } else k = 1
+
+    return k
+}
