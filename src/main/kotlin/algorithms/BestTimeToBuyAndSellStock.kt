9f36aebad47901c2c26403a475e241871b0554f7
rbrthmn <robertokenzohamano@gmail.com>
2024-06-25 13:30:57 -0300

    adding solution of problem n121

---
+++ src/main/kotlin/algorithms/BestTimeToBuyAndSellStock.kt
@@ -0,0 +1,22 @@
+package br.com.rbthmn.algorithms
+
+import br.com.rbthmn.Problem
+
+@Problem(121)
+fun maxProfit(prices: IntArray): Int {
+    var bestPriceToBuy = prices.first()
+    var bestProfit = 0
+
+    for (index in prices.indices) {
+        if (prices[index] < bestPriceToBuy) {
+            bestPriceToBuy = prices[index]
+        }
+
+        val currentProfit = prices[index] - bestPriceToBuy
+        if (bestProfit < currentProfit) {
+            bestProfit = currentProfit
+        }
+    }
+
+    return bestProfit
+}
