475edbed2967e7a556bdc0a6df5333301533b463
rbrthmn <robertokenzohamano@gmail.com>
2024-07-02 09:01:32 -0300

    adding solution of problem n28

---
+++ src/main/kotlin/algorithms/FindTheIndexOfTheFirstOccurrence.kt
@@ -0,0 +1,26 @@
+package br.com.rbthmn.algorithms
+
+import br.com.rbthmn.Problem
+
+@Problem(28)
+fun strStr(haystack: String, needle: String): Int {
+    var haystackIndex = 0
+
+    while (haystackIndex in haystack.indices) {
+        var needleIndex = haystackIndex
+        if (haystack[haystackIndex] == needle[0]) {
+            var comp = ""
+            while (needleIndex in haystack.indices && comp.length < needle.length) {
+                comp += haystack[needleIndex]
+                needleIndex++
+            }
+
+            if (comp == needle) {
+                return haystackIndex
+            }
+        }
+        haystackIndex++
+    }
+
+    return -1
+}
