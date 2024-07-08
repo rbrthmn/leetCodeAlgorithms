e24fb5609cd9d8d843a37a414fc04b012d0d7847
rbrthmn <robertokenzohamano@gmail.com>
2024-07-08 17:16:20 -0300

    adding solution of problem n 125

---
+++ src/main/kotlin/algorithms/ValidPalindrome.kt
@@ -0,0 +1,21 @@
+package br.com.rbthmn.algorithms
+
+import br.com.rbthmn.Problem
+
+@Problem(125)
+fun isPalindrome(s: String): Boolean {
+    val formattedString = s.trim().filter {
+        char -> char.isLetter() || char.isDigit()
+    }.lowercase()
+    var indexOfLastChar = formattedString.length - 1
+    var isPalindrome = true
+
+    for (char in formattedString) {
+        if (char != formattedString[indexOfLastChar]) {
+            isPalindrome = false
+        }
+        indexOfLastChar--
+    }
+
+    return isPalindrome
+}
