7848649cad058c0fd100a4d98e07bca9e941ba27
rbrthmn <robertokenzohamano@gmail.com>
2024-07-09 09:03:16 -0300

    adding solution of problem n 392

---
+++ src/main/kotlin/algorithms/IsSubsequence.kt
@@ -0,0 +1,18 @@
+package br.com.rbthmn.algorithms
+
+import br.com.rbthmn.Problem
+
+@Problem(392)
+fun isSubsequence(s: String, t: String): Boolean {
+    var sIndex = 0
+    var tIndex = 0
+
+    while (sIndex < s.length && tIndex < t.length) {
+        if (s[sIndex] == t[tIndex]) {
+            sIndex++
+        }
+        tIndex++
+    }
+
+    return sIndex == s.length
+}
