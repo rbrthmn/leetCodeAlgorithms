fd8d739ac09bdfda0ae96de6ea1d2395cf215585
rbrthmn <robertokenzohamano@gmail.com>
2024-07-25 12:04:34 -0300

    adding solution of problem n 242

---
+++ src/main/kotlin/algorithms/ValidAnagram.kt
@@ -0,0 +1,19 @@
+package br.com.rbthmn.algorithms
+
+import br.com.rbthmn.Problem
+
+@Problem(242)
+fun isAnagram(s: String, t: String): Boolean {
+    val string = StringBuilder(t)
+
+    if (s.length == t.length) {
+        for (char in s) {
+            val i = string.indexOf(char)
+            if (i != -1) {
+                string.setCharAt(i, ' ')
+            }
+        }
+    }
+
+    return s.length == t.length && string.isBlank()
+}
