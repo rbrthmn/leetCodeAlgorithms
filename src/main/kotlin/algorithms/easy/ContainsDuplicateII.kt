3be146c0c5da053ccbcffde8e4d005003c0906d4
rbrthmn <robertokenzohamano@gmail.com>
2024-07-29 13:01:43 -0300

    adding solution of problem n 219

---
+++ src/main/kotlin/algorithms/easy/ContainsDuplicateII.kt
@@ -0,0 +1,23 @@
+package br.com.rbthmn.algorithms.easy
+
+import br.com.rbthmn.Problem
+import kotlin.math.abs
+
+@Problem(219)
+fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+    fun isValid(k: Int, i: Int, j: Int): Boolean = k >= abs(i - j)
+
+    var hasValidDuplicates = false
+
+    for (index in nums.indices) {
+        if (hasValidDuplicates) break
+        for (index2 in nums.size - 1 downTo index + 1) {
+            if (nums[index] == nums[index2] && isValid(k, index, index2)) {
+                hasValidDuplicates = true
+                break
+            }
+        }
+    }
+
+    return hasValidDuplicates
+}
